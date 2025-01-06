a51e0cd6cc1c8a07b044a072691a174a111faa1d
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-06 14:59:49 +0300

    add deref for Dict types

--- nvim-types/src/dictionary.rs
+++ nvim-types/src/dictionary.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{marker::PhantomData, ops::Deref};
 
 use crate::{kvec::KVec, object::Object};
 
@@ -11,13 +11,27 @@ pub struct DictKVec {
 #[repr(transparent)]
 pub struct Dictionary(KVec<DictKVec>);
 
-#[repr(C)]
-struct KeyValPair(String, Object);
+impl Deref for Dictionary {
+    type Target = [DictKVec];
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
+    }
+}
 
 #[repr(C)]
-pub struct TypedDictionary<T, B> {
+pub struct KeyValPair(String, Object);
+
+#[repr(C)]
+pub struct TypedDictionary<B> {
     inner: KVec<KeyValPair>,
-    __p: PhantomData<(T, B)>
+    __p: PhantomData<B>,
+}
+
+impl<B> Deref for TypedDictionary<B> {
+    type Target = [KeyValPair];
+    fn deref(&self) -> &Self::Target {
+        self.inner.deref()
+    }
 }
 
 const _: () = assert!(24 == std::mem::size_of::<Dictionary>());
