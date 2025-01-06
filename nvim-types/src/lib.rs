1a42044e88fd8292748ab01ee26f09da09b0f135
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-06 15:11:59 +0300

    remove nv_type

--- nvim-types/src/lib.rs
+++ nvim-types/src/lib.rs
@@ -2,6 +2,5 @@ mod arena;
 mod array;
 mod dictionary;
 mod kvec;
-mod nv_type;
 mod object;
 mod string;
--- nvim-types/src/nv_type.rs
+++
@@ -1,5 +0,0 @@
-pub(super) trait NvType: Sealed {}
-impl <T> NvType for T where T: Sealed {}
-
-trait Sealed {}
-impl<T: NvType> Sealed for crate::kvec::KVec<T> {}
