1d78facbe015e27112b2e98caf4818cd5a64ba45
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-07 18:32:32 +0300

    arena stuff

--- nvim-types/src/arena.rs
+++ nvim-types/src/arena.rs
@@ -1,4 +1,9 @@
+use std::{marker::PhantomData, ptr::addr_of_mut};
+
+static mut ARENA: *mut Arena = core::ptr::null_mut();
+// arena_alloc_block for allocating
 #[repr(C)]
+#[derive(Clone, Copy)]
 struct Arena {
     cur_blk: *const libc::c_char,
     pos: libc::size_t,
@@ -11,11 +16,39 @@ impl Arena {
         pos: 0,
         size: 0,
     };
+
+    fn new() -> Self {
+        Self::EMPTY
+    }
+
+    unsafe fn alloc(&mut self, size: libc::size_t, align: bool) -> *mut libc::c_char {
+        unsafe { arena_alloc(self as *mut Arena, size, align) }
+    }
+
+    unsafe fn finish(&mut self) -> ArenaMem {
+        unsafe { arena_finish(self as *mut Arena) }
+    }
+}
+
+#[repr(C)]
+struct ArenaMem<'a>(*mut Self, PhantomData<&'a u8>);
+
+impl ArenaMem<'_> {
+    unsafe fn clone(&self) -> Self {
+        ArenaMem(self.0, PhantomData::<&'static u8>)
+    }
+}
+
+impl Drop for ArenaMem<'_> {
+    fn drop(&mut self) {
+        unsafe { arena_mem_free(self.clone()) };
+    }
 }
 
 // TODO: use arena to optimize performance
 // This is somewhat low priority but will be useful for large allocations
 extern "C" {
-    fn alloc_block() -> *mut libc::c_void;
-    fn free_block() -> *mut libc::c_void;
+    fn arena_alloc(arena: *mut Arena, size: libc::size_t, align: bool) -> *mut libc::c_char;
+    fn arena_finish<'a>(arena: *mut Arena) -> ArenaMem<'a>;
+    fn arena_mem_free(arena_mem: ArenaMem);
 }
