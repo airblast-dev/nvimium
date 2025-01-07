bbc0ef78dd6a244305d71d2b874b5b1d2ec6dacb
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-07 18:32:18 +0300

    make modules public

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub use nvim_funcs;
+pub use nvim_types;
+pub use plugin::plugin;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
