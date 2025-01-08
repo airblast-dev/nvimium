934bfb92e819afc88a840f04cde4f758978e1068
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-08 21:02:22 +0300

    make eval ops public

--- nvim-types/src/opts/eval_statusline.rs
+++ nvim-types/src/opts/eval_statusline.rs
@@ -5,7 +5,7 @@ use crate::{string::ThinString, window::Window, Boolean, Integer};
 masked_builder!(
     #[derive(Clone, Debug)]
     #[repr(C)]
-    struct EvalStatusLineOpts<'a> {
+    pub struct EvalStatusLineOpts<'a> {
         winid: Window,
         max_width: Integer,
         fill_char: ThinString<'a>,
