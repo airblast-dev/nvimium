b52a4ac42affdc52a720298f81d9b0c7b9f9d137
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-08 21:02:27 +0300

    add more functions

--- nvim-funcs/src/c_funcs/mod.rs
+++ nvim-funcs/src/c_funcs/mod.rs
@@ -1,4 +1,11 @@
-use nvim_types::{array::Array, dictionary::Dictionary, error::Error, func_types::KeyMapMode, string::ThinString};
+use nvim_types::{
+    array::Array,
+    error::Error,
+    func_types::KeyMapMode,
+    opts::{echo::EchoOpts, eval_statusline::EvalStatusLineOpts},
+    string::ThinString,
+    Arena,
+};
 
 extern "C" {
     pub fn nvim_create_buf(listed: bool, scratch: bool);
@@ -10,9 +17,15 @@ extern "C" {
         err: *const Error,
     );
     pub fn nvim_del_mark<'a>(name: ThinString<'a>, err: *const Error);
-    pub fn nvim_del_var<'a>(var_name:ThinString<'a>, err: *const Error);
-    // Array<Array<[String; 2]>>
-    pub fn nvim_echo<'a>(chunks: Array, history: bool, opts: Dictionary);
+    pub fn nvim_del_var<'a>(var_name: ThinString<'a>, err: *const Error);
+    // Array<Array<[String; 2]>> replace opts dict
+    pub fn nvim_echo<'a>(chunks: Array, history: bool, opts: *const EchoOpts);
     pub fn nvim_err_write<'a>(s: ThinString<'a>);
     pub fn nvim_err_writeln<'a>(s: ThinString<'a>);
+    pub fn nvim_eval_statusline<'a>(
+        s: ThinString<'a>,
+        opts: *const EvalStatusLineOpts<'a>,
+        arena: *mut Arena,
+        err: *mut Error,
+    );
 }
