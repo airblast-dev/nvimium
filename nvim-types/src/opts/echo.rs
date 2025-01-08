2afcd396732eccd26e45ea2fe939152c2c823587
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-08 20:53:47 +0300

    use builder for echo opts

--- nvim-types/src/opts/echo.rs
+++ nvim-types/src/opts/echo.rs
@@ -1,5 +1,13 @@
-#[derive(Clone, Debug, Default)]
-#[repr(C)]
-pub struct EchoOpts {
-    pub verbose: bool
-}
+use macros::{builder, fast_default};
+
+use crate::Boolean;
+
+builder!(
+    #[repr(C)]
+    #[derive(Clone, Debug)]
+    pub struct EchoOpts {
+        pub verbose: Boolean,
+    }
+);
+
+fast_default!(unsafe EchoOpts);
