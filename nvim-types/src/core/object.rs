031bac16326b1456cc68ad7bf3b42ddb4e67bcfc
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-09 17:51:10 +0300

    fix enum repr

--- nvim-types/src/core/object.rs
+++ nvim-types/src/core/object.rs
@@ -1,30 +1,48 @@
-use std::mem::ManuallyDrop;
+use std::fmt::Debug;
 
-use crate::{array::Array, dictionary::Dictionary, string::{String, ThinString}};
+use crate::{array::Array, dictionary::Dictionary, string::ThinString};
 
-
+use super::{Boolean, Float, Integer};
 
 // For layout rules see https://rust-lang.github.io/rfcs/2195-really-tagged-unions.html
 // Annoyingly isn't in any other official documentation :|
 //
 // For the enum values see src/nvim/api/private/defs.h 0.10.0 l:93
-#[derive(Debug, Default)]
-#[repr(C, u64)]
+#[derive(Default)]
+#[repr(C, u32)]
 pub enum Object {
     #[default]
     Null = 0,
-    Bool(bool),
-    Integer(i64),
-    Float(libc::c_double),
-    String(String),
+    Bool(Boolean),
+    Integer(Integer),
+    Float(Float),
+    String(ThinString<'static>),
     Array(Array),
     Dict(Dictionary),
     LuaRef,
-    Buffer()
+    Buffer,
+}
+
+impl Debug for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Object::Null => write!(f, "null"),
+            Object::Bool(b) => write!(f, "{:?}", b),
+            Object::Integer(i) => write!(f, "{:?}", i),
+            Object::Float(fl) => write!(f, "{:?}", fl),
+            Object::String(th) => write!(f, "{:?}", th),
+            Object::Array(a) => write!(f, "{:?}", a),
+            Object::Dict(d) => write!(f, "{:?}", d),
+            mut a => {
+                let ptr: *mut u32 = (&raw mut a).cast();
+                write!(f, "Unknown object: {:?}", unsafe { ptr.read() })
+            }
+        }
+    }
 }
 
 impl Clone for Object {
     fn clone(&self) -> Self {
-        todo!("impl clone for object")
+        todo!()
     }
 }
