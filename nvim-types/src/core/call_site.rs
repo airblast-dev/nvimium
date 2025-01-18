73260649f91e0ef68fddfddd9925f5978d637b2f
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-19 02:05:40 +0300

    make call site consts public

--- nvim-types/src/core/call_site.rs
+++ nvim-types/src/core/call_site.rs
@@ -1,6 +1,6 @@
 // Simplified version of src/nvim/api/private/defs.h 0.10.0 l:42
 //
 // These constants are the values passed as "channel_id" to neovim.
-const INTERNAL_CALL_MASK: u64 = (1) << (63);
-const VIML_INTERNAL_CALL: u64 = INTERNAL_CALL_MASK;
-const LUA_INTERNAL_CALL: u64 = VIML_INTERNAL_CALL + 1;
+pub const INTERNAL_CALL_MASK: u64 = (1) << (63);
+pub const VIML_INTERNAL_CALL: u64 = INTERNAL_CALL_MASK;
+pub const LUA_INTERNAL_CALL: u64 = VIML_INTERNAL_CALL + 1;
