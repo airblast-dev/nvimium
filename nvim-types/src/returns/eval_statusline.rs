2a567f34a1052162b85aee6195a71218607d4ab5
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-18 16:39:36 +0300

    fix eval statusline return type

--- nvim-types/src/returns/eval_statusline.rs
+++ nvim-types/src/returns/eval_statusline.rs
@@ -1,58 +1,60 @@
-use crate::{
-    dictionary::Dictionary,
-    kvec::KVec,
-    object::Object,
-    string::{OwnedThinString, ThinString},
-    Integer,
-};
+use crate::{dictionary::Dictionary, kvec::KVec, object::Object, string::OwnedThinString, Integer};
 
 #[derive(Debug)]
-struct EvalStatusLineDict {
+pub struct EvalStatusLineDict {
     chars: OwnedThinString,
     width: Integer,
-    highlights: KVec<HighlightItem>,
+    highlights: Option<KVec<HighlightItem>>,
 }
 
 #[derive(Debug)]
-struct HighlightItem {
+pub struct HighlightItem {
     start: Integer,
     group: OwnedThinString,
 }
 
 impl EvalStatusLineDict {
-    pub(crate) fn from_c_func_ret(mut d: Dictionary) -> Self {
+    pub fn from_c_func_ret(mut d: Dictionary) -> Self {
         let s = unsafe { d.remove("str").unwrap_unchecked().into_string_unchecked() };
         let width = unsafe {
             d.remove("width")
                 .unwrap_unchecked()
                 .into_integer_unchecked()
         };
-        let highlights = unsafe {
-            d.remove("highlights")
-                .unwrap_unchecked()
-                .into_array_unchecked()
+        let Some(highlights) = d
+            .remove("highlights")
+            .map(|ob| unsafe { Object::into_array_unchecked(ob).into_kvec() })
+        else {
+            return Self {
+                chars: s,
+                width,
+                highlights: None,
+            };
         };
 
-        let h_iter = highlights.into_kvec().into_iter();
-
-        let highlight_items = h_iter
+        let highlight_items = highlights
+            .into_iter()
             .map(|ob| {
                 let mut d = unsafe { Object::into_dict_unchecked(ob) };
-                HighlightItem {
-                    start: unsafe {
-                        d.remove("start")
-                            .unwrap_unchecked()
-                            .into_integer_unchecked()
-                    },
-                    group: unsafe { d.remove("group").unwrap_unchecked().into_string_unchecked() },
-                }
+                let start = unsafe {
+                    d.remove("start")
+                        .unwrap_unchecked()
+                        .into_integer_unchecked()
+                };
+                let group = unsafe { d.remove("group").unwrap_unchecked().into_string_unchecked() };
+                let hi = HighlightItem {
+                    start,
+                    group: OwnedThinString::from(group.as_thinstr()),
+                };
+                core::mem::forget(group);
+                hi
             })
             .collect();
 
         Self {
             chars: s,
             width,
-            highlights: highlight_items,
+            highlights: Some(highlight_items),
         }
     }
 }
