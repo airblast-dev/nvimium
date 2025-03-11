0a89bd3f1b7df630458f3c0a8fffb0e4a6e5853a
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-11 17:02:34 +0300

    add convinience methods for handles

--- nvim-types/src/core/tab_page.rs
+++ nvim-types/src/core/tab_page.rs
@@ -3,3 +3,9 @@ use super::HandleT;
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, Default)]
 pub struct TabPage(HandleT);
+
+impl TabPage {
+    pub const fn as_int(&self) -> HandleT {
+        self.0
+    }
+}
--- nvim-types/src/core/window.rs
+++ nvim-types/src/core/window.rs
@@ -3,3 +3,9 @@ use super::HandleT;
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Window(HandleT);
+
+impl Window {
+    pub const fn as_int(&self) -> HandleT {
+        self.0
+    }
+}
