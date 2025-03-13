551b839b69fe580dc0a0ed59ac0e72a83785e37e
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-13 17:24:23 +0300

    add field and func attribute macro support for masked builder

--- macros/src/masked_builder.rs
+++ macros/src/masked_builder.rs
@@ -4,7 +4,8 @@ macro_rules! masked_builder {
         $(#[$meta:meta])*
         $pub:vis struct $ident:ident$(< $( $lf:lifetime ),* >)? {
             $(
-                $(#[$field_meta:meta])*
+                $(#[field_meta = $field_meta:meta])*
+                $(#[func_meta = $func_meta:meta])*
                 $vis:vis $field:ident: $field_ty:ty
             ), *$(,)?
         }
@@ -19,7 +20,7 @@ macro_rules! masked_builder {
         }
 
         impl$(<$($lf),*>)? $ident$(<$($lf),*>),* {
-            $crate::func_gen_masked!($($field: $field_ty,)*);
+            $crate::func_gen_masked!($( $( #[$func_meta] )* $field: $field_ty,)*);
         }
     };
 }
@@ -27,13 +28,17 @@ macro_rules! masked_builder {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! func_gen_masked {
-    ($field:ident: $field_ty:ty, $($inner:ident: $inner_ty:ty,)*) => {
+    (
+        $(#[$func_meta:meta])* $field:ident: $field_ty:ty,
+        $($(#[$inner_meta:meta])* $inner:ident: $inner_ty:ty,)*
+    ) => {
+        $(#[$func_meta])*
         pub fn $field<T: Into<$field_ty>>(&mut self, $field: T) -> &mut Self {
             self.mask |= 1;
             self.$field = $field.into();
             self
         }
-        $crate::func_gen_masked_inner!(2, $($inner: $inner_ty,)*);
+        $crate::func_gen_masked_inner!(2, $( $(#[$inner_meta])* $inner: $inner_ty,)*);
     };
     () => {}
 }
@@ -41,13 +46,17 @@ macro_rules! func_gen_masked {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! func_gen_masked_inner {
-    ($mask:expr, $field:ident: $field_ty:ty, $($inner:ident: $inner_ty:ty,)*) => {
+    ( 
+        $mask:expr, $(#[$func_meta:meta])* $field:ident: $field_ty:ty, 
+        $( $(#[$inner_meta:meta])* $inner:ident: $inner_ty:ty,)*
+    ) => {
+        $( #[$func_meta] )*
         pub fn $field<T: Into<$field_ty>>(&mut self, $field: T) -> &mut Self {
             self.mask |= $mask;
             self.$field = $field.into();
             self
         }
-        $crate::func_gen_masked_inner!($mask << 1, $($inner: $inner_ty,)*);
+        $crate::func_gen_masked_inner!($mask << 1, $( $(#[$inner_meta])* $inner: $inner_ty,)*);
     };
     ($mask:expr $(,)?) => {};
 }
@@ -60,8 +69,10 @@ mod tests {
     fn builder_masked() {
         masked_builder!(
             struct A<'a> {
+                #[func_meta = doc(hidden)]
                 a: u32,
                 b: &'a str,
+                #[func_meta = doc(hidden)]
                 c: bool,
             }
         );
