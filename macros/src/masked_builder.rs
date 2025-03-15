47f65fc1ea66216fc5f045b25aafdec7cb452944
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-15 23:34:47 +0300

    cleanup masked builder and add option to skip field setter

--- macros/src/masked_builder.rs
+++ macros/src/masked_builder.rs
@@ -1,4 +1,5 @@
-// TODO: rewrite? this is getting pretty messy each time I add something
+// TODO: replace meta fragments with ident to allow better comparison in macros?
+
 
 #[macro_export]
 macro_rules! masked_builder {
@@ -6,9 +7,9 @@ macro_rules! masked_builder {
         $(#[$meta:meta])*
         $pub:vis struct $ident:ident$(< $( $lf:lifetime ),* >)? {
             $(
+                $(#[builder($skip:meta)])?
                 $(#[field_meta = $field_meta:meta])*
                 $(#[func_meta = $func_meta:meta])*
-                $(#[pre_func = $pre_func:ident])?
                 $vis:vis $field:ident: $field_ty:ty
             ), *$(,)?
         }
@@ -23,7 +24,15 @@ macro_rules! masked_builder {
         }
 
         impl$(<$($lf),*>)? $ident$(<$($lf),*>),* {
-            $crate::func_gen_masked!($( $( #[func_meta = $func_meta] )* $( #[pre_func = $pre_func] )? $field: $field_ty,)*);
+            $crate::func_gen_masked!(
+                $(
+                    $(#[builder($skip)])?
+                    $(
+                        #[func_meta = $func_meta]
+                    )*
+                    $field: $field_ty,
+                )*
+            );
         }
     };
 }
@@ -32,16 +41,46 @@ macro_rules! masked_builder {
 #[macro_export]
 macro_rules! func_gen_masked {
     (
-        $(#[func_meta = $func_meta:meta])* $(#[pre_func = $pre_func:ident])? $field:ident: $field_ty:ty,
-        $($(#[func_meta = $inner_meta:meta])* $(#[pre_func = $inner_pre_func:ident])* $inner:ident: $inner_ty:ty,)*
+        $(#[func_meta = $func_meta:meta])*
+        $field:ident: $field_ty:ty,
+        $(
+            $(#[builder($inner_skip:meta)])?
+            $(#[func_meta = $inner_func_meta:meta])*
+            $inner:ident: $inner_ty:ty,
+        )*
     ) => {
         $(#[$func_meta])*
-        pub $($pre_func)* fn $field<T: Into<$field_ty>>(&mut self, $field: T) -> &mut Self {
+        pub fn $field<T: Into<$field_ty>>(&mut self, $field: T) -> &mut Self {
             self.mask |= 2;
             self.$field = $field.into();
             self
         }
-        $crate::func_gen_masked_inner!(4, $( $(#[func_meta = $inner_meta])* $( #[pre_func = $inner_pre_func] )? $inner: $inner_ty,)*);
+        $crate::func_gen_masked_inner!(
+            4,
+            $(
+                $(#[builder($inner_skip)])?
+                $( #[func_meta = $inner_func_meta] )*
+                $inner: $inner_ty,
+            )*
+        );
+    };
+    (
+        #[builder($skip:meta)]
+        $field:ident: $field_ty:ty,
+        $(
+            $(#[builder($inner_skip:meta)])?
+            $(#[func_meta = $inner_func_meta:meta])*
+            $inner:ident: $inner_ty:ty,
+        )*
+    ) => {
+        $crate::func_gen_masked_inner!(
+            4,
+            $(
+                $(#[builder($inner_skip)])?
+                $( #[func_meta = $inner_func_meta] )*
+                $inner: $inner_ty,
+            )*
+        );
     };
     () => {}
 }
@@ -49,17 +88,50 @@ macro_rules! func_gen_masked {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! func_gen_masked_inner {
+    
     (
-        $mask:expr, $(#[func_meta = $func_meta:meta])* $( #[pre_func = $pre_func:ident] )? $field:ident: $field_ty:ty,
-        $( $(#[func_meta = $inner_meta:meta])* $(#[pre_func = $($inner_pre_func:ident),+])? $inner:ident: $inner_ty:ty,)*
+        $mask:expr,
+        #[builder($skip:meta)]
+        $field:ident: $field_ty:ty,
+        $(
+            $( #[builder($inner_skip:meta)] )?
+            $( #[func_meta = $inner_func_meta:meta] )*
+            $inner:ident: $inner_ty:ty,
+        )*
     ) => {
-        $( #[$func_meta] )*
-        pub $( $pre_func )? fn $field<T: Into<$field_ty>>(&mut self, $field: T) -> &mut Self {
+        $crate::func_gen_masked_inner!(
+            $mask << 1,
+            $(
+                $(#[builder($inner_skip)])?
+                $(#[func_meta = $inner_func_meta])* 
+                $inner: $inner_ty, 
+            )*
+        );
+    };
+    (
+        $mask:expr,
+        $(#[func_meta = $func_meta:meta])*
+        $field:ident: $field_ty:ty,
+        $(
+            $( #[builder($inner_skip:meta)] )?
+            $( #[func_meta = $inner_func_meta:meta] )*
+            $inner:ident: $inner_ty:ty,
+        )*
+    ) => {
+        $(#[$func_meta])*
+        pub fn $field<T: Into<$field_ty>>(&mut self, $field: T) -> &mut Self {
             self.mask |= $mask;
             self.$field = $field.into();
             self
         }
-        $crate::func_gen_masked_inner!($mask << 1, $( $(#[func_meta = $inner_meta])* $(#[pre_func = $($inner_pre_func),+])? $inner: $inner_ty,)*);
+        $crate::func_gen_masked_inner!(
+            $mask << 1,
+            $(
+                $(#[builder($inner_skip)])?
+                $(#[func_meta = $inner_func_meta])* 
+                $inner: $inner_ty, 
+            )*
+        );
     };
     ($mask:expr $(,)?) => {};
 }
@@ -72,10 +144,8 @@ mod tests {
     fn builder_masked() {
         masked_builder!(
             struct A<'a> {
-                #[func_meta = doc(hidden)]
                 a: u32,
                 b: &'a str,
-                #[func_meta = doc(hidden)]
                 c: bool,
             }
         );
@@ -97,6 +167,7 @@ mod tests {
         assert_eq!(c.b, "hello");
         masked_builder! {
             struct B<'a> {
+                #[func_meta = doc(hidden)]
                 a: u32,
                 b: u64,
                 c: Cow<'a, str>,
