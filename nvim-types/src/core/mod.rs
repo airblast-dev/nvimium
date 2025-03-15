2fe727441b996080551acff1ac4d5bf28a494397
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-15 12:02:57 +0300

    fmt

--- nvim-types/src/core/mod.rs
+++ nvim-types/src/core/mod.rs
@@ -1,16 +1,16 @@
 pub mod array;
-pub mod lua_ref;
 pub mod borrowed;
 pub mod buffer;
 pub mod call_site;
 pub mod dictionary;
 pub mod error;
 pub mod kvec;
+pub mod lua_ref;
+pub mod namespace;
 pub mod object;
 pub mod string;
 pub mod tab_page;
 pub mod window;
-pub mod namespace;
 
 pub type Integer = i64;
 pub type Boolean = bool;
