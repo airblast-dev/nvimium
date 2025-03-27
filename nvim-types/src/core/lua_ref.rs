7b9baa4acc6eb49ae378adbceb192aa85d4a04c7
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-27 21:45:41 +0300

    add LuaRef initializer

--- nvim-types/src/core/lua_ref.rs
+++ nvim-types/src/core/lua_ref.rs
@@ -5,6 +5,14 @@ use super::LuaRefT;
 pub struct LuaRef(pub(crate) LuaRefT);
 
 impl LuaRef {
+    /// Initialize a new LuaRef with an key
+    ///
+    /// # Safety
+    ///
+    /// Passing an invalid key can result in UB in some cases.
+    pub unsafe fn new(key: LuaRefT) -> Self {
+        Self(key)
+    }
     pub fn as_int(&self) -> LuaRefT {
         self.0
     }
