b364f39421d5adf6e880dc61044f4021b340d156
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-30 20:09:26 +0300

    add ObjectRef struct for cheap Object like data

--- nvim-types/src/core/object.rs
+++ nvim-types/src/core/object.rs
@@ -1,4 +1,8 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, marker::PhantomData, mem::ManuallyDrop};
+
+use libc::size_t;
+
+use sptr::Strict;
 
 use crate::{array::Array, dictionary::Dictionary};
 
@@ -322,4 +326,101 @@ pub enum ObjectTag {
     Buffer,
     Window,
     TabPage,
+}
+
+// TODO: this sucks, find a way to pin its size without using the magic array whilst supporting different
+// types
+/// Same as [`Object`] but accepts any type that has a valid layout for a Neovim object
+#[doc(hidden)]
+#[repr(C)]
+pub struct ObjectRef<'a, T> {
+    tag: ObjectTag,
+    val: [size_t; 3],
+    p: PhantomData<&'a T>,
+}
+
+impl<T> ObjectRef<'_, T> {
+    const SIZE: () = assert!(size_of::<T>() == 16 || size_of::<T>() == 32);
+    const OBJ_SIZE: () = assert!(size_of::<ObjectRef<T>>() == size_of::<Object>());
+
+    /// Initialize an [`ObjectRef`] with `T`
+    ///
+    /// # Safety
+    ///
+    /// Calling this function requires the tag and value to match the layout of its equal [`Object`]
+    pub unsafe fn new(tag: ObjectTag, val: &T) -> Self {
+        #[allow(clippy::let_unit_value)]
+        let _a = Self::SIZE;
+        #[allow(clippy::let_unit_value)]
+        let _a = Self::OBJ_SIZE;
+        let mut r = ObjectRef {
+            tag,
+            val: [0; 3],
+            p: PhantomData,
+        };
+        let val = unsafe { (val as *const T).cast::<ManuallyDrop<T>>().read() };
+        unsafe { r.val.as_mut_ptr().cast::<ManuallyDrop<T>>().write(val) };
+        r
+    }
+}
+
+impl<'a> From<&'a ThinString<'a>> for ObjectRef<'a, ThinString<'a>> {
+    fn from(value: &'a ThinString<'a>) -> Self {
+        // This is hand written instead of using ObjectRef::new to be able to test with miri
+        // maybe do this for other types as well?
+        let addr = value.as_ptr().expose_addr();
+        let len = value.len();
+        ObjectRef {
+            tag: ObjectTag::String,
+            val: [addr, len, 0],
+            p: PhantomData,
+        }
+    }
+}
+
+impl<'a> From<&'a Array> for ObjectRef<'a, Array> {
+    fn from(value: &'a Array) -> Self {
+        let addr = value.as_ptr().expose_addr();
+        let len = value.len();
+        let cap = value.capacity();
+        ObjectRef {
+            tag: ObjectTag::Array,
+            val: [len, cap, addr],
+            p: PhantomData,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::mem::{ManuallyDrop, transmute};
+
+    use crate::{array::Array, kvec::KVec, string::ThinString};
+
+    use super::{Object, ObjectRef};
+
+    impl<'a> ObjectRef<'a, ThinString<'a>> {
+        fn convert_back(self) -> ThinString<'a> {
+            let addr = self.val[0];
+            let ptr = sptr::from_exposed_addr(addr);
+            unsafe { ThinString::new(self.val[1], ptr) }
+        }
+    }
+
+    #[test]
+    fn object_ref_readback() {
+        let th: ThinString<'_> = ThinString::from_null_terminated(b"Hello\0");
+        let oref = ObjectRef::from(&th);
+        assert_eq!(oref.convert_back(), "Hello");
+    }
+
+    #[test]
+    fn object_ref_to_object() {
+        let obj = Object::from(Array(KVec::with_capacity(10)));
+        let arr = Array(KVec::with_capacity(10));
+        let obj_ref = ObjectRef::from(&arr);
+        let obj_reft = unsafe { transmute::<ObjectRef<_>, ManuallyDrop<Object>>(obj_ref) };
+        assert_eq!(&obj, &*obj_reft);
+    }
 }
