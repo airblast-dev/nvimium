6219dcbd4f0f8f0ffb81b489ac9cf3b52eeedb08
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-30 16:49:45 +0300

    add discriminant consts

--- nvim-types/src/core/object.rs
+++ nvim-types/src/core/object.rs
@@ -3,8 +3,8 @@ use std::fmt::Debug;
 use crate::{array::Array, dictionary::Dictionary};
 
 use super::{
-    borrowed::Borrowed, buffer::Buffer, lua_ref::LuaRef, string::OwnedThinString,
-    tab_page::TabPage, window::Window, Boolean, Float, Integer,
+    Boolean, Float, Integer, borrowed::Borrowed, buffer::Buffer, lua_ref::LuaRef,
+    string::OwnedThinString, tab_page::TabPage, window::Window,
 };
 
 // For layout rules see https://rust-lang.github.io/rfcs/2195-really-tagged-unions.html
@@ -60,42 +60,54 @@ impl Clone for Object {
 }
 
 impl Object {
-    pub fn to_bool(self) -> Option<Boolean> {
+    // Discriminant values
+    pub(crate) const D_NILL: u32 = 0;
+    pub(crate) const D_BOOL: u32 = 1;
+    pub(crate) const D_INTEGER: u32 = 2;
+    pub(crate) const D_FLOAT: u32 = 3;
+    pub(crate) const D_STRING: u32 = 4;
+    pub(crate) const D_ARRAY: u32 = 5;
+    pub(crate) const D_DICT: u32 = 6;
+    pub(crate) const D_LUAREF: u32 = 7;
+    pub(crate) const D_BUFFER: u32 = 8;
+    pub(crate) const D_WINDOW: u32 = 9;
+    pub(crate) const D_TABPAGE: u32 = 10;
+    pub fn into_bool(self) -> Option<Boolean> {
         match self {
             Object::Bool(b) => Some(b),
             _ => None,
         }
     }
 
-    pub fn to_int(self) -> Option<Integer> {
+    pub fn into_int(self) -> Option<Integer> {
         match self {
             Self::Integer(i) => Some(i),
             _ => None,
         }
     }
 
-    pub fn to_float(self) -> Option<Float> {
+    pub fn into_float(self) -> Option<Float> {
         match self {
             Object::Float(f) => Some(f),
             _ => None,
         }
     }
 
-    pub fn to_string(self) -> Option<OwnedThinString> {
+    pub fn into_string(self) -> Option<OwnedThinString> {
         match self {
             Self::String(s) => Some(s),
             _ => None,
         }
     }
 
-    pub fn to_array(self) -> Option<Array> {
+    pub fn into_array(self) -> Option<Array> {
         match self {
             Self::Array(a) => Some(a),
             _ => None,
         }
     }
 
-    pub fn to_dict(self) -> Option<Dictionary> {
+    pub fn into_dict(self) -> Option<Dictionary> {
         match self {
             Self::Dict(d) => Some(d),
             _ => None,
@@ -103,26 +115,28 @@ impl Object {
     }
 
     // TODO: complete this after adding lua integration
-    #[doc(hidden)]
-    fn to_luaref(self) -> Option<()> {
-        todo!()
+    pub fn into_luaref(self) -> Option<LuaRef> {
+        match self {
+            Self::LuaRef(lr) => Some(lr),
+            _ => None,
+        }
     }
 
-    pub fn to_buffer(self) -> Option<Buffer> {
+    pub fn into_buffer(self) -> Option<Buffer> {
         match self {
             Self::Buffer(b) => Some(b),
             _ => None,
         }
     }
 
-    pub fn to_window(self) -> Option<Window> {
+    pub fn into_window(self) -> Option<Window> {
         match self {
             Self::Window(w) => Some(w),
             _ => None,
         }
     }
 
-    pub fn to_tabpage(self) -> Option<TabPage> {
+    pub fn into_tabpage(self) -> Option<TabPage> {
         match self {
             Self::TabPage(t) => Some(t),
             _ => None,
