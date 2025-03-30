153ddde059ec519124dd56ff526d80d210e6967b
Tayfun Bocek <tayfunbocek@live.ca>
2025-03-30 17:00:38 +0300

    simplify hello world example

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,21 +1,16 @@
+
 use nvimium::{
-    nvim_funcs::wrappers::global::nvim_set_keymap,
-    nvim_types::{
-        func_types::keymap_mode::KeyMapMode, opts::set_keymap::SetKeymapOpts, string::AsThinString,
-    },
+    nvim_funcs::wrappers::global::{nvim_echo, nvim_get_hl_ns, nvim_set_hl},
+    nvim_types::{func_types::echo::Echo, opts::{echo::EchoOpts, get_hl_ns::GetHlNsOpts}},
     plugin,
 };
 
 fn hello_world() {
-    let mut opts = SetKeymapOpts::default();
-    opts.desc(c"HELLOOOOO");
-    nvim_set_keymap(
-        KeyMapMode::VISUAL,
-        c"a".as_thinstr(),
-        c"b".as_thinstr(),
-        &opts,
-    )
-    .unwrap();
+    let echo = Echo::message(c"Example Error message!");
+    let mut opts = EchoOpts::default();
+    opts.err(true);
+    nvim_echo(&echo, true, &opts).unwrap();
+    nvim_echo(&Echo::message(c"Just everyday normal message."), true, &EchoOpts::default()).unwrap();
 }
 
 plugin!(luaopen_hello_world, hello_world);
