22e59af24ad2edfed8af6d87e1b37ae35883ba63
Tayfun Bocek <tayfunbocek@live.ca>
2025-04-01 20:05:21 +0300

    remove redundant as_thinstr call

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -30,7 +30,7 @@ mod tests {
     use nvimium::nvim_funcs::vimscript::nvim_exec2;
     use nvimium::{
         nvim_test,
-        nvim_types::{dictionary::KeyValuePair, opts::exec::ExecOpts, string::AsThinString},
+        nvim_types::{dictionary::KeyValuePair, opts::exec::ExecOpts},
     };
 
     /// The nvim_test macro requires that `nvim_funcs` and `nvim_tests` are in scope!
@@ -38,7 +38,7 @@ mod tests {
     fn hello_world() {
         super::hello_world();
         let mut result = nvim_exec2(c":messages", ExecOpts::default().output(true)).unwrap();
-        let KeyValuePair { object, .. } = result.remove(c"output".as_thinstr()).unwrap();
+        let KeyValuePair { object, .. } = result.remove("output").unwrap();
         let output = object.into_string().unwrap();
 
         assert_eq!(
