769591fbabb8ccb38f6ca989c621caba7f9c9ac0
Tayfun Bocek <tayfunbocek@live.ca>
2025-04-05 18:58:18 +0300

    add constructor for Window

--- nvim-types/src/core/window.rs
+++ nvim-types/src/core/window.rs
@@ -5,6 +5,10 @@ use super::HandleT;
 pub struct Window(HandleT);
 
 impl Window {
+    pub const fn new(id: HandleT) -> Self {
+        Self(id)
+    }
+
     pub const fn as_int(&self) -> HandleT {
         self.0
     }
