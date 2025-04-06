2fa8a051204e5018b429ddbeae4fd57eb4721e93
Tayfun Bocek <tayfunbocek@live.ca>
2025-04-06 13:00:12 +0300

    add constructor for NameSpace

--- nvim-types/src/core/namespace.rs
+++ nvim-types/src/core/namespace.rs
@@ -5,6 +5,10 @@ use super::HandleT;
 pub struct NameSpace(HandleT);
 
 impl NameSpace {
+    pub const fn new(id: HandleT) -> Self {
+        Self(id)
+    }
+
     pub const fn as_int(&self) -> HandleT {
         self.0
     }
