5b1978c2d3b2a275bda005f36c97dadd581c402d
Tayfun Bocek <tayfunbocek@live.ca>
2025-05-17 16:27:00 +0300

    add error return example

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,10 +1,12 @@
+use std::error::Error;
+
 use nvimium::{
     nvim_funcs::global::echo,
-    nvim_types::{Integer, func_types::echo::Echo, opts::echo::EchoOpts},
+    nvim_types::{func_types::echo::Echo, opts::echo::EchoOpts},
     plugin,
 };
 
-fn hello_world() {
+fn hello_world() -> Result<(), Box<dyn Error>> {
     let echo_msg = Echo::message(c"Example Error message!");
     let mut opts = EchoOpts::default();
     // once set neovim prints out the message with the error highlighting
@@ -12,13 +14,14 @@ fn hello_world() {
     // depending on the config the message will likely be displayed in red
     opts.err(true);
     // Both echo's have history as true so we can read it in testing.
-    echo(&echo_msg, true, &opts).unwrap();
+    echo(&echo_msg, true, &opts)?;
     echo(
         &Echo::message(c"Just an everyday normal message."),
         true,
         &EchoOpts::default(),
-    )
-    .unwrap();
+    )?;
+
+    Ok(())
 }
 
 // create our lua entrypoint
@@ -38,7 +41,7 @@ mod tests {
     fn hello_world() {
         // After calling this function the message log should contain the messages sent from our
         // function.
-        super::hello_world();
+        super::hello_world().unwrap();
         let mut result = exec2(c":messages", ExecOpts::default().output(true)).unwrap();
         let KeyValuePair { object, .. } = result.remove("output").unwrap();
         let output = object.into_string().unwrap();
