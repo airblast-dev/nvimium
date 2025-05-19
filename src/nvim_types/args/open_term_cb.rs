1f3c03332fa7f885a0df7b53c6d3f74538f736c8
Tayfun Bocek <tayfunbocek@live.ca>
2025-05-19 11:06:31 +0300

    fix argument read for OpenTermOnInputArgs
    Since these functions are called from C, the stack is expected to be
    dirty.
    
    To avoid problems:
    - Maintain a cleaner stack
    - Use negative indices in access
    - Read the arguments in reverse

--- src/nvim_types/args/open_term_cb.rs
+++ src/nvim_types/args/open_term_cb.rs
@@ -1,4 +1,6 @@
-use crate::nvim_types::{Buffer, FromLua, Integer, ThinString};
+use mlua_sys::{lua_pop, lua_tolstring, lua_type, lua_typename};
+
+use crate::nvim_types::{Buffer, FromLua, Integer, String, ThinString};
 
 pub struct OpenTermOnInputArgs<'a> {
     pub src: ThinString<'a>,
@@ -7,15 +9,25 @@ pub struct OpenTermOnInputArgs<'a> {
     pub data: ThinString<'a>,
 }
 
-impl <'a>OpenTermOnInputArgs<'a> {
-    pub(crate) unsafe fn pop(l: *mut mlua_sys::lua_State) -> crate::nvim_types::lua::core::Result<OpenTermOnInputArgs<'a>> {
-        Ok(unsafe {
-            Self {
-                src: ThinString::pop(l)?,
-                term: Integer::pop(l)?,
-                buf: Buffer::pop(l)?,
-                data: ThinString::pop(l)?,
-            }
-        })
+impl<'a> OpenTermOnInputArgs<'a> {
+    pub(crate) unsafe fn pop(
+        l: *mut mlua_sys::lua_State,
+    ) -> crate::nvim_types::lua::core::Result<OpenTermOnInputArgs<'a>> {
+        unsafe {
+            let data = ThinString::pop(l)?;
+            lua_pop(l, 1);
+            let buf = Buffer::pop(l)?;
+            lua_pop(l, 1);
+            let term = Integer::pop(l)?;
+            lua_pop(l, 1);
+            let src = ThinString::pop(l)?;
+            lua_pop(l, 1);
+            Ok(Self {
+                src,
+                term,
+                buf,
+                data,
+            })
+        }
     }
 }
