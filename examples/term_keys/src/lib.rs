849feb386fb4f439da332a8d626c9c2cc0916ee1
Tayfun Bocek <tayfunbocek@live.ca>
2025-05-19 11:07:53 +0300

    add term_keys example

---
+++ examples/term_keys/src/lib.rs
@@ -0,0 +1,36 @@
+use std::error::Error;
+
+use nvimium::{
+    nvim_funcs::global::{create_buf, del_keymap, echo, open_term, set_current_buf, set_keymap},
+    nvim_types::{
+        func_types::{echo::Echo, keymap_mode::KeyMapMode},
+        opts::{echo::EchoOpts, open_term::OpenTermOpts, set_keymap::SetKeymapOpts},
+    },
+    plugin,
+};
+
+fn term_keys() -> Result<(), Box<dyn Error>> {
+    let buf = create_buf(true, false)?;
+    open_term(
+        buf,
+        OpenTermOpts::default().on_input(|args| {
+            echo(
+                &Echo::message(args.data),
+                true,
+                EchoOpts::default().err(true),
+            )
+            .unwrap();
+        }),
+    )?;
+    set_current_buf(buf)?;
+    Ok(())
+}
+
+// create our lua entrypoint
+plugin!(luaopen_term_keys, term_keys);
+
+#[cfg(feature = "testing")]
+mod tests {
+    // testing keymaps can get very complicated, instead you are encouraged to test a function that
+    // acts upon a keymap seperately
+}
