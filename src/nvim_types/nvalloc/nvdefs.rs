351c0d8b47d36199b8ced05549346050af99738b
Tayfun Bocek <tayfunbocek@live.ca>
2025-05-21 22:41:57 +0300

    comment

--- src/nvim_types/nvalloc/nvdefs.rs
+++ src/nvim_types/nvalloc/nvdefs.rs
@@ -5,7 +5,7 @@ unsafe extern "C" {
     pub static E_OUTOFMEM: *const c_char;
     /// Attempts to recover from an allocation error by freeing extra memory that neovim is holding.
     ///
-    /// Intended to be used with this libraries xmalloc definition
+    /// Intended to be used with this libraries xmalloc definition and its exported allocator
     #[cold]
     pub fn try_to_free_memory();
     #[cold]
