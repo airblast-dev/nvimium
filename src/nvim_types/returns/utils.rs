f3b381356fb30e55d37dc8ada91260caa3d069f0
Tayfun Bocek <tayfunbocek@live.ca>
2025-05-23 01:18:30 +0300

    cleanup key extractor

--- src/nvim_types/returns/utils.rs
+++ src/nvim_types/returns/utils.rs
@@ -1,30 +1,66 @@
-use std::{ffi::CStr, mem::ManuallyDrop};
+use std::{mem::MaybeUninit, ptr::NonNull};
 
 use crate::nvim_types::{Dict, Object};
 
+/// Removes the keys without dropping them and
 #[inline(never)]
-pub(crate) fn remove_keys(
-    keys: &'static [&'static CStr],
+pub(crate) unsafe fn remove_keys(
+    len: usize,
+    mut keys: NonNull<&'static str>,
+    mut out: NonNull<MaybeUninit<Object>>,
     d: &mut Dict,
-    cleanup: Option<fn([ManuallyDrop<Object>; 20])>,
-) -> Result<[ManuallyDrop<Object>; 20], &'static CStr> {
-    let len = keys.len();
+    missing_key: Option<fn(&'static str) -> Option<Object>>,
+) -> Result<(), &'static str> {
     let mut i = 0;
     // an array length of 20 should enough for all return values
-    let mut objects = [const { ManuallyDrop::new(Object::Null) }; 20];
     // based on the std libraries comments this is unlikely to unroll which is good for binary size
     while i < len {
-        let key = keys[i];
-        let Some(val) = d.remove_skip_key_drop(key) else {
-            if let Some(f) = cleanup {
-                f(objects);
-            }
+        let key = unsafe { keys.read() };
+        let Some(val) = d
+            .remove_skip_key_drop(key)
+            .or_else(|| missing_key.and_then(|f| f(key)))
+            .map(MaybeUninit::new)
+        else {
             return Err(key);
         };
+        unsafe {
+            out.write(val);
+            keys = keys.add(1);
+            out = out.add(1);
+        };
 
-        objects[i] = ManuallyDrop::new(val);
         i += 1;
     }
 
-    Ok(objects)
+    Ok(())
+}
+
+macro_rules! extract_owned_kv {
+    (
+        $collector:ident,
+        $s:ident, $matches:pat $(, $extract:expr)? $(, $fallback:expr)?
+    ) => {
+        if let Some($matches) = d.remove_skip_key_drop(stringify!($s)) {
+            $extract($ident);
+        } else {
+            $fallback($ident);
+        }
+    };
+}
+
+macro_rules! extract_ref_kv {
+    (
+        $collector:ident,
+        $s:ident, $matches:pat $(, $extract:expr)? $(, $fallback:expr)?
+    ) => {
+        if let Some($matches) = d
+            .remove_skip_key_drop(stringify!($s))
+            .map(::std::mem::ManuallyDrop::new)
+            .deref()
+        {
+            $extract($ident);
+        } else {
+            $fallback($ident);
+        }
+    };
 }
