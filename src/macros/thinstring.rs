d0617495324d252fa9bbd877149d5da5d64d176d
Tayfun Bocek <tayfunbocek@live.ca>
2025-06-06 01:54:59 +0300

    cleanup th macro

--- src/macros/thinstring.rs
+++ src/macros/thinstring.rs
@@ -1,9 +1,15 @@
+use crate::nvim_types::ThinString;
+
 #[macro_export]
 macro_rules! th {
     ($v:vis $id:ident = $s:literal) => {
-        $v const $id: $crate::nvim_types::ThinString<'static> = $crate::nvim_types::ThinString::from_null_terminated(::std::concat!($s, "\0").as_bytes());
+        $v const $id: $crate::nvim_types::ThinString<'static> = $crate::macros::thinstring::th_f(::std::concat!($s, "\0"));
     };
     ($s:literal) => {
-        $crate::nvim_types::ThinString::from_null_terminated(::std::concat!($s, "\0").as_bytes())
+        $crate::macros::thinstring::th_f(::std::concat!($s, "\0"))
     }
+}
+
+pub(crate) const fn th_f<'a>(s: &'a str) -> ThinString<'a> {
+    ThinString::from_null_terminated(s.as_bytes())
 }
