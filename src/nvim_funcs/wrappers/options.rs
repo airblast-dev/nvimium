2ba80fb2dc373ef81f48b5b3e06de1e805074a86
Tayfun Bocek <tayfunbocek@live.ca>
2025-06-08 11:18:44 +0300

    add set_option_value

--- src/nvim_funcs/wrappers/options.rs
+++ src/nvim_funcs/wrappers/options.rs
@@ -1,12 +1,13 @@
 use thread_lock::call_check;
 
 use crate::{
-    macros::tri::{tri_nc, tri_ret},
+    macros::tri::{tri_ez, tri_nc, tri_ret},
     nvim_funcs::c_funcs::options::{
         nvim_get_all_options_info, nvim_get_option_info2, nvim_get_option_value,
+        nvim_set_option_value,
     },
     nvim_types::{
-        AsThinString, CALLBACK_ARENA, Error, Object,
+        AsThinString, CALLBACK_ARENA, Channel, Error, Object,
         opts::option::OptionOpt,
         returns::options_info::{OptionInfo, OptionsInfo},
     },
@@ -42,11 +43,27 @@ pub fn get_options_info2<TH: AsThinString>(name: TH) -> Result<OptionInfo, Error
     })
 }
 
-pub fn get_option_value<'a, TH: AsThinString>(name: TH, opt: &mut OptionOpt<'a>) -> Result<Object, Error> {
+pub fn get_option_value<'a, TH: AsThinString>(
+    name: TH,
+    opts: &mut OptionOpt<'a>,
+) -> Result<Object, Error> {
     call_check();
 
     tri_nc! {
-        err; 
-        unsafe { nvim_get_option_value(name.as_thinstr(), opt, &raw mut err) }; 
+        err;
+        unsafe { nvim_get_option_value(name.as_thinstr(), opts, &raw mut err) };
+    }
+}
+
+pub fn set_option_value<'a, TH: AsThinString>(
+    name: TH,
+    value: Object,
+    opts: &mut OptionOpt<'a>,
+) -> Result<(), Error> {
+    call_check();
+
+    tri_ez! {
+        err;
+        unsafe { nvim_set_option_value(Channel::LUA_INTERNAL_CALL, name.as_thinstr(), (&value).into(), opts, &raw mut err) };
     }
 }
