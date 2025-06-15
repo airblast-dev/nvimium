f32d6cb00469b45941dbe5d54d52cc7a9f479db2
Tayfun Bocek <tayfunbocek@live.ca>
2025-06-15 17:08:45 +0300

    disable swapfiles for false negatives in tests

--- src/test_macro_utils.rs
+++ src/test_macro_utils.rs
@@ -1,10 +1,13 @@
-use std::{ffi::OsStr, path::Path, ffi::c_int};
+use std::{ffi::OsStr, ffi::c_int, path::Path};
 
 use mlua_sys::lua_State;
 use nvim_test::set_test_panic_hook;
 use thread_lock::{init_main_lua_ptr, scoped};
 
-use crate::{nvim_funcs::global::get_var, nvim_types::TRACKED_ARENA};
+use crate::{
+    nvim_funcs::{global::get_var, options::set_option_value},
+    nvim_types::{Object, TRACKED_ARENA, opts::option::OptionOpt},
+};
 
 #[doc(hidden)]
 pub unsafe fn test_body(l: *mut lua_State, test_call: fn(), exit_call: fn()) -> c_int {
@@ -21,6 +24,8 @@ pub unsafe fn test_body(l: *mut lua_State, test_call: fn(), exit_call: fn()) -> 
                     test_out.as_thinstr().as_slice(),
                 ));
                 set_test_panic_hook(test_out.to_path_buf());
+                set_option_value(c"swapfile", Object::Bool(true), &mut OptionOpt::default())
+                    .unwrap();
                 test_call();
                 #[allow(static_mut_refs)]
                 TRACKED_ARENA.reset_arena();
