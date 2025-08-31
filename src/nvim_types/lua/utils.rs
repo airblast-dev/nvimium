f89716f4d1f6c90ca5ebea83ebf795d1e21dbe84
Tayfun Bocek <tayfunbocek@live.ca>
2025-09-01 01:15:06 +0300

    collaps cond

--- src/nvim_types/lua/utils.rs
+++ src/nvim_types/lua/utils.rs
@@ -24,35 +24,32 @@ use super::{LuaInteger, core::FromLuaErr};
 pub(crate) unsafe fn handle_callback_err_ret(l: *mut lua_State, err: &dyn Error) {
     let mut s = NvString::default();
     write!(s, "Error: {}", &err).unwrap();
-    if let Err(echo_err) = echo(&Echo::message(s), true, EchoOpts::default().err(true)) {
-        if echo(
+    if let Err(echo_err) = echo(&Echo::message(s), true, EchoOpts::default().err(true))
+        && echo(
             &Echo::message(echo_err.as_thinstr()),
             true,
             EchoOpts::default().err(true),
         )
         .is_err()
-        {
-            // TODO: while i doubt its possible, find a safer solution
-            // since we should never reach this branch give a more helpful error message instead
-            // of attempting to chug along
-            debug_assert!(
-                false,
-                "Nvimium Internal Error: Unable to write error message to neovim (this likely a bug in nvimium)"
-            );
-            unsafe {
-                // lua_error performs a long jump over rust stack frames so
-                // this is inherently unsound
-                // (currently not UB but safety isn't guaranteed by spec)
-                // ideally we never reach this branch
-                if lua_checkstack(l, 1) == 0 {
-                    panic!("Nvimium: Not enough stack space to push error message")
-                }
-                ThinString::from_null_terminated(
-                    b"Nvimium: Error whilst writing error to neovim\0",
-                )
-                .push(l);
-                lua_error(l);
+    {
+        // TODO: while i doubt its possible, find a safer solution
+        // since we should never reach this branch give a more helpful error message instead
+        // of attempting to chug along
+        debug_assert!(
+            false,
+            "Nvimium Internal Error: Unable to write error message to neovim (this likely a bug in nvimium)"
+        );
+        unsafe {
+            // lua_error performs a long jump over rust stack frames so
+            // this is inherently unsound
+            // (currently not UB but safety isn't guaranteed by spec)
+            // ideally we never reach this branch
+            if lua_checkstack(l, 1) == 0 {
+                panic!("Nvimium: Not enough stack space to push error message")
             }
+            ThinString::from_null_terminated(b"Nvimium: Error whilst writing error to neovim\0")
+                .push(l);
+            lua_error(l);
         }
     }
 }
